4969d382dbbb40ddb7854682928aab06b7d58c9a
bzeier <43791891+bzeier@users.noreply.github.com>
2021-12-15 10:43:39 +0100

    hide title on mobile

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ function Header({ searchvalue, onChangeMethod }) {
 
     return (
         <header className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
-            <h1 className=" ml-2 text-gray-700 text-2xl">Game Engine Explorer</h1>
+            <h1 className=" ml-2 text-gray-700 text-2xl sm:hidden">Game Engine Explorer</h1>
             <div className="focus-within:text-gray-600 focus-within:shadow-md mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-500 rounded-lg">
                 <Icon name="search" size="3xl" color="gray"/>
                 <input type="text" value={searchvalue} onChange={e => onChangeMethod(e.target.value)}  placeholder="Search engine or game" className="flex-grow px-5 text-base bg-transparent outline-none"/>
